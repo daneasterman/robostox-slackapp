810565ecfe999a4768f2bb44e532cc0683d7fb50
Daniel Easterman <daniel.easterman@gmail.com>
2021-06-18 16:54:04 +0100

    wip get stock data

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import yfinance as yf
 from datetime import datetime, time, timedelta
 from slack_bolt import App
 from slack_sdk import WebClient
@@ -9,34 +10,57 @@ load_dotenv()
 SLACK_BOT_TOKEN = str(os.getenv('SLACK_BOT_TOKEN'))
 SLACK_SIGNING_SECRET = str(os.getenv('SLACK_SIGNING_SECRET'))
 
-client = WebClient(SLACK_BOT_TOKEN)
+def percent_change(start_point, end_point):
+    return((float(end_point)-start_point)/abs(start_point))*100.00
 
-def get_convo_id():    
-    channel_name = "test-alerts"
-    convo_id = None
-    try:
-        for result in client.conversations_list():
-            if convo_id is not None:
-                break
-            for channel in result["channels"]:
-                if channel["name"] == channel_name:
-                    convo_id = channel["id"]                    
-                    return convo_id                    
+def get_single_stock():
+    # Hardcode as MSFT for now, need to catch error here, what if API fails?
+    stock = yf.Ticker("MSFT")
+    
+    # General info
+    long_name = stock.info['longName']
+    symbol = stock.info['symbol']
+    price = stock.info['regularMarketPrice']
+    marketcap = stock.info['marketCap']
+        
+    
+    # Week percent change:
+    week_df = stock.history(period="5d")
+    week_start_price = week_df.Open.iat[0]
+    week_end_price = week_df.Open.iat[-1]
+    week_percent_change = percent_change(week_start_price, week_end_price)    
+    print(round(week_percent_change, 2))
 
-    except SlackApiError as e:
-        print(f"Error: {e}")
 
-def publish_message():
-    convo_id = get_convo_id()
-    try:    
-        result = client.chat_postMessage(
-            channel=convo_id,
-            text="Hello world!"
-        )    
-        print(result)
+get_single_stock()
 
-    except SlackApiError as e:
-        print(f"Error: {e}")
+# client = WebClient(SLACK_BOT_TOKEN)
+# def get_convo_id():    
+#     channel_name = "test-alerts"
+#     convo_id = None
+#     try:
+#         for result in client.conversations_list():
+#             if convo_id is not None:
+#                 break
+#             for channel in result["channels"]:
+#                 if channel["name"] == channel_name:
+#                     convo_id = channel["id"]                    
+#                     return convo_id                    
+
+#     except SlackApiError as e:
+#         print(f"Error: {e}")
+
+# def publish_message():
+#     convo_id = get_convo_id()
+#     try:    
+#         result = client.chat_postMessage(
+#             channel=convo_id,
+#             text="Hello world!"
+#         )
+#         print(result)
+
+#     except SlackApiError as e:
+#         print(f"Error: {e}")
 
 
 # Start Slack App
---
+++ schedule_message.py
@@ -0,0 +1,20 @@
+def schedule_message():
+    convo_id = get_convo_id()
+    every_friday = datetime.now().astimezone()
+    while every_friday.weekday() != 4:
+        every_friday += timedelta(1)
+
+    scheduled_time = time(hour=11, minute=30)
+    scheduled_timestamp = datetime.combine(every_friday, scheduled_time).strftime('%s')
+    
+    try:
+        result = client.chat_scheduleMessage(
+            channel=convo_id,
+            text="Publish future Friday message test!",
+            post_at=scheduled_timestamp
+        )
+        print(result)
+    except SlackApiError as e:
+        print(f"Error: {e}")
+
+schedule_message()
