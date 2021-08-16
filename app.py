66557e106d03217bddf260f3133ec752798ae332
Daniel Easterman <daniel.easterman@gmail.com>
2021-08-16 16:34:40 +0100

    Fix UI unch display bug for weekly update

--- app.py
+++ app.py
@@ -60,18 +60,24 @@ def ack_ticker_select(ack, action):
 		stock_dict = {
 			'long_name': stock.info['longName'],
 			'current_price': round(current_price, 2),
-			'week_change': "" if week_change == 0 else week_change
+			'week_change': 0 if week_change == 0 else week_change
 			}
 		stocks_list.append(stock_dict)		
 	publish_message(stocks_list)
 
 def natural_lang(percent):
-	if percent == "":
-		return "is unchanged"
+	if percent == 0:
+		return "unchanged"
 	elif percent > 0:
-		return "up "
+		return "up"
 	elif percent < 0:
-		return "down "
+		return "down"
+
+def show_percent(week_change):
+	if week_change == 0:
+		return ""
+	else:		
+		return f"`{week_change}%` "
 
 client = WebClient(SLACK_BOT_TOKEN)
 def publish_message(stocks_list):
@@ -90,11 +96,12 @@ def publish_message(stocks_list):
 	blocks.append(top_divider)
 
 	for s in stocks_list:
+		week_change = s['week_change']		
 		main_info = {
 			"type": "section",
 			"text": {
-				"type": "mrkdwn",			
-				"text": f"*{s['long_name']}* is trading at `${s['current_price']}` and is *{natural_lang(s['week_change'])}*`{s['week_change']}%` for the week."
+				"type": "mrkdwn",
+				"text": f"*{s['long_name']}* is trading at `${s['current_price']}` and is *{natural_lang(week_change)}* {show_percent(week_change)} for the week."
 			},
 		}		
 		blocks.append(main_info)
