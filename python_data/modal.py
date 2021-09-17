42457e77d26da503afac6c62db7790949e40bc72
Daniel Easterman <daniel.easterman@gmail.com>
2021-09-17 10:09:00 +0100

    Copy changes to modal

--- python_data/modal.py
+++ python_data/modal.py
@@ -3,7 +3,7 @@ from python_data.local_tickers import local_tickers
 modal = [
 		{
 			"type": "section",
-			"block_id": "stocks-modal",
+			"block_id": "stocks_modal",
 			"text": {
 				"type": "mrkdwn",
 				"text": "Hello, Assistant to the Regional Manager Dwight! Here are a few Robostox settings.\n\n"
@@ -12,9 +12,10 @@ modal = [
 		{
 			"label": {
 				"type": "plain_text",
-				"text": "Please choose your notification options:"
+				"text": "Switch your real-time notifications off or on:"
 			},
 			"type": "input",
+			"block_id": "radio_input",
 			"element": {
 				"action_id": "toggle_realtime",
 				"type": "radio_buttons",
@@ -46,9 +47,10 @@ modal = [
 		{
 			"label": {
 				"type": "plain_text",
-				"text": "Please choose your notification options:\n\n"
+				"text": "Select your stocks to receive real-time filing notifications:\n\n"
 			},
 			"type": "input",
+			"block_id": "multi_select_input",
 			"element": {
 				"action_id": "ticker_select",
 				"type": "multi_static_select",
