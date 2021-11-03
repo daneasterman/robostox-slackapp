a0fa21d33d84da572eef2e3e6887c618a89109f2
Daniel Easterman <daniel.easterman@gmail.com>
2021-11-03 18:55:58 +0000

    Move utils folder back

--- python_data/app_errors.py
+++ python_data/app_errors.py
@@ -1,11 +1,22 @@
-def plain_api_error(user_symbol):	
-	return f"Sorry, the ticker symbol {user_symbol} was not found. Are you sure you spelt it correctly? You can find more stock ticker symbols on: Yahoo Finance."
+def plain_stock_error(user_symbol):	
+	return f"Sorry, the ticker symbol {user_symbol} was not found. Are you sure you spelt it correctly? You can find more stock ticker symbols o Yahoo Finance."
 
-def rich_api_error(user_symbol):
+def plain_crypto_error(user_symbol):	
+	return f"Sorry, the symbol {user_symbol} was not found. Are you sure you spelt it correctly? You can find more stock ticker symbols on CoinGecko."
+
+def rich_stock_error(user_symbol):
 	rich_error_text = [{
 				"type": "section",
 				"text": {"type": "mrkdwn", "text": f"Sorry, the ticker symbol *{user_symbol}* was not found. Are you sure you spelt it correctly? You can find more stock ticker symbols on: <https://finance.yahoo.com/|Yahoo Finance>"}
 				}]
 	return rich_error_text
 
-generic_error_text = "Sorry, something has gone wrong. Please contact the creator of this app here: daniel.easterman@gmail.com for help or more information."
+def rich_crypto_error(user_symbol):
+	rich_error_text = [{
+				"type": "section",
+				"text": {"type": "mrkdwn", "text": f"Sorry, the crypto symbol *{user_symbol}* was not found. Are you sure you spelt it correctly? You can find more crypto symbols on: <https://www.coingecko.com/|CoinGecko>"}
+				}]
+	return rich_error_text
+
+# TO DO: CHANGE TO ROBOSTOX DEDICATED EMAIL:
+generic_error_text = "Sorry, something has gone wrong. Please contact the creator of the app here: daniel.easterman@gmail.com for help or more information."
