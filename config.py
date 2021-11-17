b2e1d9196994e767d3f1ad8496a1aa7793bddf4c
Daniel Easterman <daniel.easterman@gmail.com>
2021-11-17 16:46:01 +0000

    Specify elif production

--- config.py
+++ config.py
@@ -9,15 +9,15 @@ DEPLOY_ENV = os.getenv('DEPLOY_ENV')
 
 if DEPLOY_ENV == "development":
 	SLACK_SIGNING_SECRET = os.getenv('DEV_SLACK_SIGNING_SECRET')
-else:
+elif DEPLOY_ENV == "production":
 	SLACK_SIGNING_SECRET = os.getenv('PROD_SLACK_SIGNING_SECRET')
 
 if DEPLOY_ENV == "development":
 	SLACK_CLIENT_ID = os.getenv('DEV_SLACK_CLIENT_ID')
-else:
+elif DEPLOY_ENV == "production":
 	SLACK_CLIENT_ID = os.getenv('PROD_SLACK_CLIENT_ID')
 
 if DEPLOY_ENV == "development":
 	SLACK_CLIENT_SECRET = os.getenv('DEV_SLACK_CLIENT_SECRET')
-else:
+elif DEPLOY_ENV == "production":
 	SLACK_CLIENT_SECRET = os.getenv('PROD_SLACK_CLIENT_SECRET')
