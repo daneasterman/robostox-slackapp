71c3c7b5532f8906a0c7cd66320e4645ef65a181
Daniel Easterman <daniel.easterman@gmail.com>
2022-01-26 11:24:46 +0000

    Added database file

---
+++ database.py
@@ -0,0 +1,40 @@
+import logging
+import os
+from typing import Optional
+
+import sqlalchemy
+from slack_sdk.oauth.installation_store import InstallationStore
+from slack_sdk.oauth.installation_store.sqlalchemy import SQLAlchemyInstallationStore
+from slack_sdk.oauth.state_store.sqlalchemy import SQLAlchemyOAuthStateStore
+from sqlalchemy.engine import Engine
+from config import SLACK_SIGNING_SECRET, SLACK_CLIENT_ID, SLACK_CLIENT_SECRET, SQLALCHEMY_DATABASE_URI
+
+engine: Optional[Engine] = None
+installation_store: Optional[InstallationStore] = None
+
+logger = logging.getLogger(__name__)
+database_url = SQLALCHEMY_DATABASE_URI
+
+logging.getLogger("sqlalchemy.engine").setLevel(logging.DEBUG)
+engine = sqlalchemy.create_engine(database_url)
+installation_store = SQLAlchemyInstallationStore(
+    client_id=SLACK_CLIENT_ID,
+    engine=engine,
+    logger=logger,
+)
+oauth_state_store = SQLAlchemyOAuthStateStore(
+    expiration_seconds=120,
+    engine=engine,
+    logger=logger,
+)
+
+def run_db_migration():
+	try:
+			engine.execute("select count(*) from slack_bots")
+	except Exception as _:
+			installation_store.metadata.create_all(engine)
+			oauth_state_store.metadata.create_all(engine)
+
+run_db_migration()
+
+
